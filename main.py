81d81b1d7b9258c8b325d50cde15e25816223ef4
Greg L <futurefunk@gmail.com>
2020-01-24 02:48:29 -0500

    prof asked for more logical decomposition, went back to drawing board.
    code has 2 known bugs.
    
    1) entering zero for menu input crashes the program.
    zero breaks the validator.
    
    2) there is an index checking problem with lists of length 1

--- main.py
+++ main.py
@@ -1,97 +1,117 @@
 """
-Greg L
+Greg Lundberg
 94283312
-1/21/20
+1/23/20
 
-truffle_manager demonstrates functional programming techniques through
-simulation of an order-taking system for a truffle business.
+truffle manager is state of the art inventory management for the truffle
+industry - disguised as exercise in functional decomposition.
 
-an array of inventory record is passed in and used to generate a submenu
-of the items in inventory. the user may select among options
-to order items from the inventory, restock, or view the updated data
-after return from the adjust() function, which is used for both
-ordering and restocking.
+parallel arrays of items and corresponding quantities are passed in. a main
+menu is displayed, prompting the user to select among options to Order, Restock
+or View the inventory. inventory data is used to generate submenus in which
+item quantities are adjusted.
+
+a set of base functions is created to handle menu display, user input,
+input validation. these are composed in get_opt(), which provides the menus
+with a single function to display their options and return a valid selection
+from the user.
 
 """
+items = ['Milk chocolate', 'Dark chocolate', 'Hazelnut']
+quantities = [1000, 1000, 1000]
 
 
-inventory = [["Milk chocolate", 1000], ["Dark chocolate", 1000], ["Hazelnut", 1000]]
+def truffle_man(items,quantities):
 
-# truffleManager is the control function which sets up the main menu
-# and calls the other functions according to user input.
-# it accepts an array of inventory in Record format
-def truffle_manager(inv):
+	options = ['ORDER', ' RESTOCK', ' PRINT', ' QUIT']
 
-    mainMenu = ['ORDER', ' RESTOCK', ' PRINT', ' QUIT']
+	opt = get_opt(options, lambda x: 0 <= x <= len(options))
 
-    while True:
-        num = menu(mainMenu)
-        if num == 0:
-            inv = adjust(inv)
-        elif num == 1:
-            inv = adjust(inv, True)
-        elif num == 2:
-            show(inv)
-        else:
-            break  # can only be Quit (3)
-
-    return inv
+	if opt == 1:
+		order(items, quantities)
+	elif opt == 2:
+		restock(items, quantities)
+	elif opt == 3:
+		print_inventory(items, quantities)
+	else:
+		return items, quantities
 
 
-# menu accepts a list of inventory items and presents them to the user as
-# numbered options (i+1). user will be prompted until a valid number is
-# entered, which is then returned as the [zero-based] index of the item chosen.
-def menu(options):
-    while True:
-        for i in range(len(options)):
-            print(f'{i + 1}) {options[i]}')
 
-        num = int(input('Please enter the number of your choice: ')) - 1  # adjust to index
-        if num in range(0, len(options)):  # validate selection
-            break
+# subtract quantity per order
+def order(items, quantities):
+	opt = get_opt(items, lambda x: 1 <= x <= len(items)) - 1
+	num = get_opt(None, lambda x: 1 <= x, f'How many of {items[opt]}? ')
 
-    return num
+	if num <= quantities[opt]:
+		quantities[opt] -= num
+		print('Your order was successfully placed.\n')
+	else:
+		print("I'm sorry, your order could not be placed.\n")
 
-
-# adjust updates inventory data up or down according to orders or restocks.
-# the restock flag determines the operation.
-def adjust(inv, restock=False):
-    # inventory items, qty's to separate arrays
-    items = []
-    quantities = []
-    for i in inv:
-        items.append(i[0])
-        quantities.append(i[1])
-    # item to be adjusted (0-based index)
-    num = menu(items)
-
-    while True:
-        n = int(input(f'How many of {items[num]}? '))
-        if n > 0:
-            if restock:
-                quantities[num] += n
-                print('That item has been restocked.\n')
-                break
-            if n <= quantities[num]:
-                quantities[num] -= n
-                print('Your order was successfully placed.\n')
-                break
-            elif n > quantities[num]:
-                print("I'm sorry, your order could not be placed.\n")
-                break
-    # reconstruct inventory data structure for  return
-    arr = []
-    for i in range(len(items)):
-        arr.append([items[i], quantities[i]])
-    return arr
+	truffle_man(items, quantities)
 
 
-# print inventory to console
-def show(inv):
-    print('{:<20}{:<20}'.format('Item', 'Quantity'))
-    for i in inv:
-        print(f'{i[0]:20}{i[1]:<20}')
-    print('\n')
+# replenish quantity per order
+def restock(items, quantities):
+	opt = get_opt(items, lambda x: 1 <= x <= len(items)) - 1
+	num = get_opt(None, lambda x: 1 <= x, f'How many of {items[opt]}? ')
+
+	quantities[opt] += num
+	print('That item has been restocked.\n')
+
+	truffle_man(items, quantities)
 
 
-truffle_manager(inventory)
+# print table of inventory
+def print_inventory(items, quantities):
+	print('\n{:<20}{:<20}'.format('Item', 'Quantity'))
+	for i in range(len(items)):
+		print(f'{items[i]:20}{quantities[i]:<20}')
+
+	truffle_man(items, quantities)
+
+
+# composed function to handle menu display, input, validation
+# returns 1-based index of menu options
+def get_opt(options, validate_func, text='Please enter the number of your choice: '):
+	if options:
+		display(options)
+	# prompt until valid selection
+	opt = False
+	while opt is False:
+		opt = validate(prompt(text), validate_func)
+	return opt
+
+
+# prints n string args one per line
+# list args passed to menu generator
+def display(*args):
+	for i in args:
+		if type(i) == list:
+			display_menu(i)
+		else:
+			print(i)
+
+
+# prints numbered list as menu (1, 2...)
+def display_menu(options):
+	for i in range(len(options)):
+		print(f'{i + 1}) {options[i]}')
+
+
+# requests input from user
+def prompt(text='Please enter the number of your choice: '):
+	return input(text)
+
+
+# check validity of input. return valid integer, otherwise False
+def validate(val, func):
+	val = int(val)
+	if func(val):
+		return val
+	else:
+		return False
+
+
+truffle_man(items, quantities)
